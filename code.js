8e931738d4b9b3b26a0c88dda84851fac4d2805d
jattiemyaseen <yaseenjattiem0000@gmail.com>
2022-06-20 13:17:40 +0200

    well

---
+++ code.js
@@ -0,0 +1,18 @@
+let array = [1,2,3,4,5,6];
+
+
+
+const iterator = array.values();
+let one = iterator.next().value;
+let two = iterator.next().value;
+let three = iterator.next().value;
+let four = iterator.next().value;
+let five = iterator.next().value;
+
+
+document.querySelector('#array').innerHTML = array;
+document.querySelector('#arr1').innerHTML = one;
+document.querySelector('#arr2').innerHTML = two;
+document.querySelector('#arr3').innerHTML = three;
+document.querySelector('#arr4').innerHTML = four;
+document.querySelector('#arr5').innerHTML = five;
